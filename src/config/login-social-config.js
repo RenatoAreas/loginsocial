d7f46b70513a12ca317fe722e080e82de9c2a3de
Renato Areas <rsareas93@gmail.com>
2022-07-12 12:13:48 -0300

    first commit

---
+++ src/config/login-social-config.js
@@ -0,0 +1,7 @@
+export const getGoogleID = () => {
+    return '20960426803-jou6o5b0ntlinp93nfiebtupvq76k1o6.apps.googleusercontent.com';
+}
+
+export const getFacebookID = () => {
+    return '1025500974770149';
+}
